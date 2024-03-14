527fa3f5245ab8e10e19a9dd1e18fd2745304698
Elizbeth <elizabethadebayo269@gmail.com>
2024-03-14 11:03:12 +0100

    adding code to generate a random number

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
     println!("please input your guess.");
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to red line");
     println!("you guessed: {guess}");
-
-    let x = 5;
-    let y = 10;
-    println!("x = {x} and y + 2 = {}", y + 2);
 }
 //enum is a type with many state.
 //each possible state is a variant
