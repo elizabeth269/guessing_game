093a9a861380d37bbc231c6ad1873b14a1d6e084
Elizbeth <elizabethadebayo269@gmail.com>
2024-03-14 11:42:13 +0100

    remove the user from seeing the secret number and added a few changes to the file

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,29 @@ use std::io; //enum that has variant ok(success) or err(tell you why it failed)
 fn main() {
     println!("guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
-    println!("please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to red line");
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("you guessed: {guess}");
+    //println!("The secret number is: {secret_number}");
+    loop {
+        println!("please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to red line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    //comparing values
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You Win"),
+        println!("you guessed: {guess}");
+
+        //comparing values
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+        }
     }
 }
 //enum is a type with many state.
